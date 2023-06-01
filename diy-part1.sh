91bfa08752a73ff23a1bd960542726c0ef137500
wu1094366146 <61652427+wu1094366146@users.noreply.github.com>
2023-06-01 18:34:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
